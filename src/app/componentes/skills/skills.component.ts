2739d5577b972a18fe7e65b9ef2e8a268463cd30
fbenitez <fernanbenitez@hotmail.com.ar>
2022-08-12 02:18:39 -0300

    .js tcanvas visible en skills

--- src/app/componentes/skills/skills.component.ts
+++ src/app/componentes/skills/skills.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 
-declare var particlesJS: any;
+
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
@@ -12,7 +12,8 @@ export class SkillsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    particlesJS.load('particles-js', '../assets/particles.json', null);
+   
   }
 
+  
 }
